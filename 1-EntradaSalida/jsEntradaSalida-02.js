536897ec0bbef0c75ec6af92b380fcc17b3d4e31
BarraganAna <anabarragan3312@gmail.com>
2020-07-17 17:11:06 -0300

    Nuevo Cambio

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,8 +3,17 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+	// declarar una variable
 	var vNombre;
-	vNombre = prompt("Nombre","Ingrese un Nombre");
+	var vApellido; 
+    // hacer una asignacion
+
+	vNombre = prompt("Nombre","Ingrese un Nombre"); // Asignardo prompt a vNombre
 	alert(vNombre);
+	
+	// Pisar una varible
+	vApellido = prompt("Apellido","Ingrese un Apellido");
+	alert(vApellido);
+	
 }
 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -3,7 +3,10 @@ Debemos lograr tomar un dato por 'PROMPT'
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
-{
-	alert("ok");
+{ var vnombre;
+	
+	vnombre = prompt ("ingrese un dato");
+	document.getElementById("txtIdNombre").value = vnombre;
+	
 }
 
